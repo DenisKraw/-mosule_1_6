60f8393cdfe016ba3d98b36e435d2b7b335bce7b
DenisKraw <Chiks1187@gmail.com>
2024-11-04 16:48:26 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -5,8 +5,9 @@ my_dict['Natali']=1999
 print(my_dict['Natali'])
 my_dict.update({'Nikolaj':1977,
                 'Anna':1998})
-my_dict.pop('Anton')
-print(my_dict)
+print(my_dict.get('Nona'))
+a=my_dict.pop('Anton')
+print(a)
 my_set={1,2,2,3,True,'Katia','Katia'}
 my_set.update({6,'Nikolaj'})
 my_set.discard(2)
